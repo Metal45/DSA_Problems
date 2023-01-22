cdb7a74130b4c23d70ac907a0acc6867d7eb9a63
Varchasva Arora <Varchasv45@gmail.com>
2023-01-22 20:13:20 +0530

    Linked List 2 CN

---
+++ 01_LinkedList/08_MergeTwoSortedLL.cpp
@@ -0,0 +1,33 @@
+Node *mergeTwoSortedLinkedLists(Node *head1, Node *head2)
+{
+  // 1st approach iterative approach 
+  // make a new dummy node and then compare the value of head1 and head2 and connect the dummy node to the smaller value;
+  
+  Node *newHead = new Node(-1);
+  Node *p3 = newHead;
+
+  while(head1 != NULL && head2 != NULL){
+    if(head1 -> data < head2 -> data){
+      p3 -> next = head1;
+      head1 = head1 -> next;
+    }else{
+      p3 -> next = head2;
+      head2 = head2 -> next;
+    }
+     p3 = p3 -> next;
+  }
+
+  while(head1 != NULL){
+    p3 -> next = head1;
+    head1 = head1 -> next;
+    p3 = p3 -> next;
+  }
+
+  while (head2 != NULL) {
+    p3->next = head2;
+    head2 = head2->next;
+    p3 = p3->next;
+  }
+
+  return newHead -> next;
+}
