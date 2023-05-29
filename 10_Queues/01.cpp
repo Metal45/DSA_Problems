08937a26bf694ef0cd50e6fbd79fe0c15b715553
Varchasva Arora <97362029+Metal45@users.noreply.github.com>
2023-05-29 11:24:49 +0530

    Queues

---
+++ 10_Queues/01.cpp
@@ -0,0 +1,16 @@
+// Implementation of queue using vector
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class queue{
+    public:
+    int front, rear;
+    vector<int> arr;
+    
+}
+
+int main(){
+
+}
