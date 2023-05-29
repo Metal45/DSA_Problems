0f388634f7c2788d0ba890d8606348df0cf8f9c9
Varchasva Arora <97362029+Metal45@users.noreply.github.com>
2023-05-29 11:25:31 +0530

    Heaps

---
+++ 13_Heaps/01_Implementation.cpp
@@ -0,0 +1,141 @@
+#include <iostream>
+using namespace std;
+
+class Heap{
+    public:
+    int *arr;
+    int size;
+    int heapsize;
+
+    Heap(int Tsize){
+        size = Tsize + 1;
+        arr = new int[Tsize];
+        heapsize = 0;
+    }
+
+
+    void insertKey(int val){
+       
+        if(heapsize >= size - 1){
+            cout << "Overflow, heap is full" << endl;
+            return ;
+        }
+
+        heapsize++;
+        arr[heapsize] = val;
+        int childIndex = heapsize;
+
+        while(childIndex > 1){
+            int parentIndex = childIndex/2;
+
+            if(arr[childIndex] > arr[parentIndex]){
+                swap(arr[childIndex], arr[parentIndex]);
+                childIndex = parentIndex;
+            }
+            else{
+                break;
+            }
+
+        }
+    }
+
+
+    void deleteKey(){
+        arr[1] = arr[heapsize];
+        heapsize--;
+        int i = 1;
+        
+        while(i < heapsize){
+            int lchild = 2*i;
+            int rchild = 2*i + 1;
+            int largest = i;
+
+            if(lchild < heapsize && arr[largest] < arr[lchild]){
+                largest = lchild;
+            }
+
+            if(rchild < heapsize && arr[largest] < arr[rchild]){
+                largest = rchild;
+            }
+
+            if(largest == i){
+                return;
+            }else{
+                swap(arr[i], arr[largest]);
+                i = largest;
+            }
+        }
+    }
+};
+
+
+void heapify(int *arr, int heapsize, int i){
+    int index = i;
+    int lchild = 2*i;
+    int rchild = 2*i + 1;
+    int largest = i;
+
+    if(lchild < heapsize && arr[largest] < arr[lchild]){
+        largest = lchild;
+    }
+
+    if(rchild < heapsize && arr[largest] < arr[rchild]){
+        largest = rchild;
+    }
+
+    if(largest == index){
+        return;
+    }else{
+        swap(arr[index], arr[largest]);
+        index = largest;
+        heapify(arr, heapsize, index);
+    }
+}
+
+void BuildHeap(int *arr, int size){
+    for(int i = size/2; i > 0; i--){
+        heapify(arr, size, i);
+    }
+}
+
+
+int main(){
+    // Heap H1(6);
+    // H1.insertKey(50);
+    // H1.insertKey(30);
+    // H1.insertKey(70);
+    // H1.insertKey(40);
+    // H1.insertKey(80);
+    // H1.insertKey(100);
+    // H1.insertKey(90);
+    
+    // for(int i = 1; i <= H1.heapsize; i++){
+    //     cout << H1.arr[i] << " ";
+    // }
+
+    // cout << endl;
+    // H1.deleteKey();
+    // for(int i = 1; i <= H1.heapsize; i++){
+    //     cout << H1.arr[i] << " ";
+    // }
+
+    int n;
+    cin >> n;
+    int *arr2 = new int[n];
+
+    for(int i = 0; i < n; i++){
+        cin >> arr2[i];
+    }
+    
+    for(int i = 1; i < n; i++){
+        cout << arr2[i] << " ";
+    }
+
+    cout << endl;
+
+    BuildHeap(arr2, n);
+
+    for(int i = 1; i < n; i++){
+        cout << arr2[i] << " ";
+    }
+}
---
+++ 13_Heaps/02_HeapSort.cpp
@@ -0,0 +1,61 @@
+//  Heap Sort -> O(NlogN)
+// You can sort an array by converting it into an heap, by doing that the root node will always be maximum
+
+// steps to sort the array:
+// 1. build heap from the array -> O(N)
+// 2. swap root with last node -> O(1)
+// 3. decrease the size of the heap and heapify first node -> O(LogN)
+// 4. repeat it until size of heap is 1 -> O(N)
+
+// doing so the last element in the array will always be the root which is largest so it will be sorted in ascending order
+
+#include <iostream>
+using namespace std;
+
+void heapify(int *arr, int heapsize, int i){
+    int index = i;
+    int lchild = 2*i;
+    int rchild = 2*i + 1;
+    int largest = index;
+
+    if(lchild < heapsize && arr[largest] < arr[lchild]){
+        largest = lchild;
+    }
+
+    if(rchild < heapsize && arr[largest] < arr[rchild]){
+        largest = rchild;
+    }
+
+    if(largest == index){
+        return;
+    }else{
+        swap(arr[index], arr[largest]);
+        index = largest;
+        heapify(arr, heapsize, index);
+    }
+}
+
+void BuildHeap(int *arr, int size){
+    for(int i = size/2; i > 0; i--){
+        heapify(arr, size, i);
+    }
+}
+
+void HeapSort(int *arr, int n){
+    int i = n - 1;
+    while(i != 1){
+        swap(arr[i], arr[1]);
+        n--;
+        i--;
+        heapify(arr, n, 1);
+    }
+}
+
+int main(){
+    int arr[] = {-1, 100, 50, 60, 140, -52, 1, 20, 30};
+    BuildHeap(arr, 9);
+    HeapSort(arr, 9);
+    for(int i = 0; i < 9; i++){
+        cout << arr[i] << " ";
+    }
+}
