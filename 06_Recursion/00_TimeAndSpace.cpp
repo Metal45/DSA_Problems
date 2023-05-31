499c64f416128c21bb1f4dad7895aa94f29c9010
Varchasva Arora <97362029+Metal45@users.noreply.github.com>
2023-06-01 00:35:44 +0530

    Recursion

---
+++ 06_Recursion/00_TimeAndSpace.cpp
@@ -0,0 +1,9 @@
+// TIme and Space Complexity of Recursive Solutions
+
+// Time Complexity -> Time taken by an algorithm as a function of input (how many comparisions or operations are done with respect to input)
+// Space Complexity -> Maximum space required by a algorithm (is vector considered as an auxillary space if only used for returning the answer)
+
+// Make recursive tree of the fucntion 
+
+// -> TIME COMPLEXTIY can be calculated by the number of calls made in the recursive tree 
+// -> maximum depth of the tree is the SPACE COMPLEXITY of the solution
