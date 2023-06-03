0552137f84a29efaecb4ac51f8916c8ce29c1ba0
Varchasva Arora <97362029+Metal45@users.noreply.github.com>
2023-06-03 21:46:07 +0530

    Strings

--- 04_CharArrays_and_Strings/15_LongestCommonPrefix.cpp
+++ 04_CharArrays_and_Strings/15_LongestCommonPrefix.cpp
@@ -1,3 +1,7 @@
+        // pick one string and match all the characters with all the rest of the strings unitl it does not match with any character in a string else increase the index and return the characters of the intitital string till that index as the answer
+        // time complexity O(M*N) -> M size of first string and N -> number of strings to be nmtched 
+
+
 string longestCommonPrefix(vector<string>& strs) {
     string curr = strs[0];
     int j = 0;
