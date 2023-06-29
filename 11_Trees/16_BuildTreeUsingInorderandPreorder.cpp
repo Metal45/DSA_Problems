69369d5422170edf85854c08a248c5f84b8e8cde
Varchasva Arora <97362029+Metal45@users.noreply.github.com>
2023-06-29 17:47:04 +0530

    Trees

--- 11_Trees/16_BuildTreeUsingInorderandPreorder.cpp
+++ 11_Trees/16_BuildTreeUsingInorderandPreorder.cpp
@@ -54,3 +54,40 @@ public:
         return solve(preorder, inorder, preIndex, inorderStart, inorderEnd);
     }
 };
+
+
+
+
+// we can use map to reduce the time complexity of searching the index in inorder 
+
+map<int, int> mp;
+void mapping(vector<int>& inorder){
+    for(int i=0;i<inorder.size();i++)
+        mp[inorder[i]]=i; 
+}
+
+TreeNode *solve(vector<int>& preorder, vector<int>& inorder, int &preIndex, int inorderStart, int inorderEnd){
+    if(preIndex >= preorder.size() || inorderStart > inorderEnd){
+        return NULL;
+    }
+
+    int element = preorder[preIndex++];
+    TreeNode *root = new TreeNode(element);
+
+    int pos = mp[element];
+
+    root -> left = solve(preorder, inorder, preIndex, inorderStart, pos - 1);
+    root -> right = solve(preorder, inorder, preIndex, pos + 1, inorderEnd);
+
+    return root;
+}
+
+TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+    int preIndex = 0;
+    int inorderStart = 0;
+    int inorderEnd = inorder.size() - 1;
+
+    mapping(inorder);
+
+    return solve(preorder, inorder, preIndex, inorderStart, inorderEnd);
+}
