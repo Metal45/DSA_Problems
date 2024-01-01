f649c5e3a2fc232aeec15ce2c7bd6abb7ea852c8
Varchasva Arora <97362029+Metal45@users.noreply.github.com>
2024-01-02 03:53:55 +0530

    NCR

--- Optimisation_Tricks/04_Ncr_optimised.cpp
+++ Optimisation_Tricks/04_Ncr_optimised.cpp
@@ -2,14 +2,20 @@
 #define ll long long
 using namespace std;
 
-
+// We use the concept of pascal traingle to find the ncr value
+// ncr = n! / (r! * (n - r)!)
 
 int main(){
-
-    int t;
-    cin>>t;
-    while(t--){
-        
+    // ncr in this vector is stored as ncr[n][r]
+    vector<vector<int>> ncr(1001, vector<int>(1001, 0));
+    for(int i = 0; i <= 1000; i++){
+        for(int j = 0; j <= i; j++){
+            if(j == 0 || j == i){
+                ncr[i][j] = 1;
+            } else {
+                // ncr = n-1cr-1 + n-1cr
+                ncr[i][j] = ncr[i - 1][j - 1] + ncr[i - 1][j];
+            }
+        }
     }
-
 }
