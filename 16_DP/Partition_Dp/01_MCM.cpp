e08d827c937680060e1f747f29f02e24420cc393
Varchasva Arora <97362029+Metal45@users.noreply.github.com>
2024-03-05 22:27:48 +0530

    Dp

---
+++ 16_DP/Partition_Dp/01_MCM.cpp
@@ -0,0 +1,14 @@
+// MULTIPLICATION OF TWO MATRICES 
+
+
+// Number of operations in multiplying two matrices 
+
+// suppose their are Two
+// matrix A of size -> M X N
+// matrix B of size -> N X P
+// Total number of operations in forming one element of resultant matrix ->
+    // For forming one element of resultant matrix you need to multiply 
+    // the ith row of matrix A with ith column of matrix B and add them 
+    // which n multiplications and n - 1 additions that is a total of 2n - 1 operations
+// Size of the resultant matrix will be M X P 
+// Total number of operations will be = total operations for getting one element 
